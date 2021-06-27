d7fdee5f5cb727b2c26b419cdd45d1b9ccad375c
Harley Trung <harley@coderpush.com>
2021-06-27 14:20:25 +0700

    feat: test CredsEnv.load method

--- lib/creds_env.rb
+++ lib/creds_env.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
+require "rails"
 require_relative "creds_env/version"
 
-module CredsEnv
+class CredsEnv
   class Error < StandardError; end
+
   # Your code goes here...
+
+  include Singleton
+
+  def self.load(rails_application)
+    # turn credentials that are IN_CAPS to be ENV["IN_CAPS"] env var
+    config = rails_application.credentials.config
+    config.keys.grep(/[A-Z]/).each do |var|
+      ENV[var.to_s] = config[var].to_s
+    end
+  end
 end
--- spec/creds_env_spec.rb
+++ spec/creds_env_spec.rb
@@ -1,11 +1,73 @@
 # frozen_string_literal: true
 
+ENV["RAILS_ENV"] = "test"
+require "action_controller/railtie"
+
 RSpec.describe CredsEnv do
-  it "has a version number" do
-    expect(CredsEnv::VERSION).not_to be nil
+  before do
+    class RailsTestApp < Rails::Application
+      config.logger = Logger.new(nil)
+      config.eager_load = false # avoid warning
+      config.require_master_key = true
+    end
+
+    RailsTestApp.initialize!
+
+    update_credentials_yml_enc({})
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  after do
+    %i[RailsTestApp].each do |const|
+      Object.send(:remove_const, const)
+    end
+
+    Rails.application = nil
+  end
+
+  let(:sample_creds) do
+    {
+      secret_key_base: "abc123",
+      DATABASE_URL: "postgres://localhost:5432/example_development",
+      AWS_ACCESS_KEY_ID: "aws123",
+      AWS_SECRET_ACCESS_KEY: "aws123",
+    }
+  end
+
+  context "check Rails app setup" do
+    it "has a version number" do
+      expect(CredsEnv::VERSION).not_to be nil
+    end
+
+    it "ENV['RAILS_ENV'] exists" do
+      expect(ENV["RAILS_ENV"]).to eq "test"
+    end
+
+    it "sets up Rails creds correctly" do
+      update_credentials_yml_enc(sample_creds)
+      expect(Rails.application.credentials.config).to eq(sample_creds)
+    end
+  end
+
+  describe ".load" do
+    before do
+      update_credentials_yml_enc(sample_creds)
+      CredsEnv.load(Rails.application)
+    end
+
+    it "sets variables like DATABASE_URL" do
+      expect(ENV["DATABASE_URL"]).to eq sample_creds.dig(:DATABASE_URL)
+      expect(ENV["AWS_ACCESS_KEY_ID"]).to eq sample_creds.dig(:AWS_ACCESS_KEY_ID)
+      expect(ENV["AWS_SECRET_ACCESS_KEY"]).to eq sample_creds.dig(:AWS_SECRET_ACCESS_KEY)
+    end
+
+    it "ignores variables like secret_key_base" do
+      expect(ENV["secret_key_base"]).to eq nil
+    end
+  end
+
+  def update_credentials_yml_enc(conf)
+    Rails.application.credentials.change do |path|
+      File.open(path, "w") { |f| f.write(conf.to_yaml) }
+    end
   end
 end
