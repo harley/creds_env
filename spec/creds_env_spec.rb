1c95751ea8b646ed4543dc62204b9ff821e7d65e
Harley Trung <harley@coderpush.com>
2021-06-27 13:45:30 +0700

    feat: change test/ to spec/

---
+++ spec/creds_env_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CredsEnv do
+  it "has a version number" do
+    expect(CredsEnv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
